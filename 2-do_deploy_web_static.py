9a191f5c39f0a5ab12f40d52598c670ce8a61777
Shimapondo <williamnamabende@gmail.com>
2024-02-05 07:14:30 -0800

    deploying an archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-""" the script that contains the necessary functions for deploying a
-    website to a remote server
 """
-from fabric.api import env, put, run
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
+
+from fabric.api import put, run, env
 from os.path import exists
 env.hosts = ['100.26.254.163', '100.24.72.49']
 
 
 def do_deploy(archive_path):
-    """ function deploys an archive in the versions directory to a server
-    """
+    """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
     try:
-        archive_name = archive_path.split('/')[-1]
-        archive_name_no_extension = archive_name.split('.')[0]
-        link = '/data/web_static/current'
-        dest_folder = f'/data/web_static/releases/{archive_name_no_extension}'
-
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run(f'mkdir -p {dest_folder}/')
-        run(f'tar -xzf /tmp/{archive_name} -C {dest_folder}')
-        run(f'rm /tmp/{archive_name}')
-        run(f'mv {dest_folder}/web_static/* {dest_folder}')
-        run(f'rm -rf {dest_folder}/web_static/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run(f'ln -s {dest_folder}/ {link}')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception:
+    except:
         return False
---
+++ 2-do_deploy_web_statics.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" the script that contains the necessary functions for deploying a
+    website to a remote server
+"""
+from fabric.api import env, put, run
+from os.path import exists
+env.hosts = ['100.26.254.163', '100.24.72.49']
+
+
+def do_deploy(archive_path):
+    """ function deploys an archive in the versions directory to a server
+    """
+    if exists(archive_path) is False:
+        return False
+    try:
+        archive_name = archive_path.split('/')[-1]
+        archive_name_no_extension = archive_name.split('.')[0]
+        link = '/data/web_static/current'
+        dest_folder = f'/data/web_static/releases/{archive_name_no_extension}'
+
+        put(archive_path, '/tmp/')
+        run(f'mkdir -p {dest_folder}/')
+        run(f'tar -xzf /tmp/{archive_name} -C {dest_folder}')
+        run(f'rm /tmp/{archive_name}')
+        run(f'mv {dest_folder}/web_static/* {dest_folder}')
+        run(f'rm -rf {dest_folder}/web_static/')
+        run('rm -rf /data/web_static/current')
+        run(f'ln -s {dest_folder}/ {link}')
+        return True
+    except Exception:
+        return False
